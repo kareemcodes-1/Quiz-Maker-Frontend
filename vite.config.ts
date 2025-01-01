f0904cee6cde7eda3ac488851a7b28cc6b5ec631
Kareem Braimoh <kareembrai222@gmail.com>
2025-01-01 05:42:33 +0100

    Thanks

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from "path"
 
 
-// const BACKENDURL = process.env.VITE_BACKEND_URL as string;
+const BACKENDURL = import.meta.env.VITE_BACKEND_URL as string;
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -16,7 +16,7 @@ export default defineConfig({
   server: {
     proxy: {
        '/api': {
-           target: "https://productivity-app-server-429hbhnu5.vercel.app/",
+           target: "https://productivity-app-server-429hbhnu5.vercel.app//",
            changeOrigin: true,
        }
     }
